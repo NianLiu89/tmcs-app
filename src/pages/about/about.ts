23eb3782cfbca689edc247509dc80e366dbf2a32
Nian Liu <nliu@bol.com>
2017-04-23 14:02:10 +0200

    Revert "use about page to experiment with websocket"
    This reverts commit a5d99a4e93a7b9bb7070f73f1e856f7696854b0b.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,43 +1,12 @@
-import { Component, OnInit } from "@angular/core";
-import * as $ from "jquery";
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
-
-export class AboutPage implements OnInit {
-
-
+export class AboutPage {
 
   constructor() {
 
-  }
-
-  ngOnInit(): void {
-    this.connect();
-  }
-
-
-  public connect() {
-    let ws = new WebSocket('ws://localhost:8080/chat');
-    console.log('socket');
-    ws.onopen = function () {
-      console.log('opening socket');
-    };
-    ws.onmessage = function (data) {
-      console.log('new message!');
-      console.log(data);
-    };
-
-    $("#btn").click(function () {
-      console.log('sending message to server');
-      ws.send('hello websocket from client');
-    });
-
-    $("#testId").on("click", function () {
-      console.log('sending message to server');
-      ws.send('hello websocket from client');
-    });
   }
 }
