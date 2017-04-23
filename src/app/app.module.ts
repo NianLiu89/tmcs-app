bff355ef148d026eeb65b62beda387f75fe257f6
Nian Liu <nliu@bol.com>
2017-04-23 14:02:19 +0200

    Revert "add websocket, but not working yet"
    This reverts commit d980a325ab8ac375fb955a696bb9fc3810e3577e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AboutPage } from "../pages/about/about";
 import { DataService } from "./data/data.service";
 import { DummyDataService } from "./data/dummy.data.service";
 import { HttpModule } from "@angular/http";
-import { WebSocketService } from "./websocket/websocket.service";
 
 @NgModule({
   declarations: [
@@ -25,7 +24,7 @@ import { WebSocketService } from "./websocket/websocket.service";
     TemperaturePage,
     AboutPage
   ],
-  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, DataService, DummyDataService, WebSocketService]
+  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, DataService, DummyDataService]
 })
 export class AppModule {
 }
--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -2,36 +2,21 @@ import { Injectable, OnInit } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { DataPoint } from "./datapoint";
 import { Observable } from "rxjs/Rx";
-import { Subject } from "rxjs/Subject";
-import { WebSocketService } from "../websocket/websocket.service";
 
 @Injectable()
 export class DataService implements OnInit {
 
     // private url: string = "http://192.168.2.1:8080/datapoints";
     private url: string = "http://localhost:8080/datapoints";
-
     private data: any;
-
-    private message: Subject<string>;
-
-    constructor(private http: Http, private webSocketService: WebSocketService) {
-        <Subject<string>>webSocketService
-            .connect("ws://localhost:8080/chat/topic/msg")
-            .map((response: MessageEvent): string => {
-                console.log("message");
-                console.log(JSON.stringify(response));
-                console.log(response);
-                return JSON.stringify(response);
-            });
-
-
+    constructor(private http: Http) {
         console.log("DataService constructor");
         let timer = Observable.timer(0, 5000);
         timer.subscribe(() => {
         })
 
         this.getData();
+
     }
 
     ngOnInit(): void {
